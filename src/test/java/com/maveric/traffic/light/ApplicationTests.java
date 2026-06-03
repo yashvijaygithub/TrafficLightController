278c354def8f77016767fbe7faa61c236e749be1
yashvijaygithub <yashvijaybc@gmail.com>
2026-06-03 13:49:31 +0530

    NOJIRA - gitignore and other classes

---
+++ src/test/java/com/maveric/traffic/light/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.traffic.light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
